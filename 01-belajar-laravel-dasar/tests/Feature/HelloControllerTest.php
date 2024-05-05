08db1789ec089821865b5ea37268b0835d63d2bc
Samuel Ivan <maztersamzz@gmail.com>
2024-05-05 16:15:24 +0700

    Laravel Dasar 24 Request

--- 01-belajar-laravel-dasar/tests/Feature/HelloControllerTest.php
+++ 01-belajar-laravel-dasar/tests/Feature/HelloControllerTest.php
@@ -15,4 +15,14 @@ class HelloControllerTest extends TestCase
     {
         $response = $this->get('/controller/hello/Eko')->assertSeeText('Halo Eko');
     }
+
+    public function testRequest()
+    {
+        $this->get('/controller/hello/request', [
+            "Accept" => "plain/text"
+        ])->assertSeeText('/controller/hello/request')
+            ->assertSeeText('http://localhost/controller/hello/request')
+            ->assertSeeText('GET')
+            ->assertSeeText('plain/text');
+    }
 }
