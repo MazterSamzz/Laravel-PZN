9dcb6a9b5ed70227195b497127f4b64cf01d43aa
Samuel Ivan <maztersamzz@gmail.com>
2024-05-05 13:40:30 +0700

    Laravel Dasar 21 Route Parameter

--- 01-belajar-laravel-dasar/tests/Feature/RoutingTest.php
+++ 01-belajar-laravel-dasar/tests/Feature/RoutingTest.php
@@ -24,4 +24,30 @@ class RoutingTest extends TestCase
         $this->get('tidakadalagi')->assertSeeText('404 by Programmer Zaman Now');
         $this->get('ups')->assertSeeText('404 by Programmer Zaman Now');
     }
+
+    public function testRouteParameter()
+    {
+        $this->get('/products/1')->assertSeeText('Product 1');
+
+        $this->get('/products/2')->assertSeeText('Product 2');
+
+        $this->get('/products/1/items/xxx')->assertSeeText('Product 1, Item xxx');
+    }
+
+    public function testRouteParameterRegex()
+    {
+        $this->get('/categories/1')->assertSeeText('Category 1');
+
+        $this->get('/categories/ivan')->assertSeeText('404 by Programmer Zaman Now');
+    }
+
+    public function testRouteParameterOptional()
+    {
+        $this->get('/users')->assertSeeText('User 404');
+    }
+
+    public function testRouteConflict(){
+        $this->get('/conflict/budi')->assertSeeText("Conflict budi");
+        $this->get('/conflict/eko')->assertSeeText('Conflict Eko Kurniawan Khannedy');
+    }
 }
