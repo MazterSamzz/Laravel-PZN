3c1a2cd3e7280654e669e0c641667a0fd428ea68
Samuel Ivan <maztersamzz@gmail.com>
2024-05-11 22:28:07 +0700

    Membuat Web Login & Todolist 04 Membuat User Service

---
+++ 02-laravel-todolist/tests/Feature/UserServiceTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserServiceTest extends TestCase
+{
+    private UserService $userService;
+    /**
+     * A basic feature test example.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->userService = $this->app->make(UserService::class);
+    }
+
+    public function testSample(): void
+    {
+        self::assertTrue(true);
+    }
+}
