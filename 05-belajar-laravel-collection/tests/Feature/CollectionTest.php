334f31159d150ffbf271357cade45667ca85346b
Samuel Ivan <maztersamzz@gmail.com>
2024-05-23 16:34:52 +0700

    Laravel Collection 16 Take dan Skip

--- 05-belajar-laravel-collection/tests/Feature/CollectionTest.php
+++ 05-belajar-laravel-collection/tests/Feature/CollectionTest.php
@@ -298,4 +298,40 @@ class CollectionTest extends TestCase
         $result = $collection->slice(3, 2);
         $this->assertEqualsCanonicalizing([4, 5], $result->all());
     }
+
+    public function testTake(): void
+    {
+        $collection = collect([1, 2, 3, 1, 2, 3, 1, 2, 3]);
+
+        $result = $collection->take(3);
+        $this->assertEqualsCanonicalizing([1, 2, 3], $result->all());
+
+        $result = $collection->takeUntil(function ($value, $key) {
+            return $value == 3;
+        });
+        $this->assertEquals([1, 2], $result->all());
+
+        $result = $collection->takeWhile(function ($value, $key) {
+            return $value < 3;
+        });
+        $this->assertEquals([1, 2], $result->all());
+    }
+
+    public function testSkip(): void
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        $result = $collection->skip(3);
+        $this->assertEqualsCanonicalizing([4, 5, 6, 7, 8, 9], $result->all());
+
+        $result = $collection->skipUntil(function ($value, $key) {
+            return $value == 3;
+        });
+        $this->assertEqualsCanonicalizing([3, 4, 5, 6, 7, 8, 9], $result->all());
+
+        $result = $collection->skipWhile(function ($value, $key) {
+            return $value < 3;
+        });
+        $this->assertEqualsCanonicalizing([3, 4, 5, 6, 7, 8, 9], $result->all());
+    }
 }
