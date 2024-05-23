3dc81dfeb0b5f67e25f9398d7f99033c407a52a7
Samuel Ivan <maztersamzz@gmail.com>
2024-05-23 12:58:59 +0700

    Laravel Collection 08 Zipping

--- 05-belajar-laravel-collection/tests/Feature/CollectionTest.php
+++ 05-belajar-laravel-collection/tests/Feature/CollectionTest.php
@@ -95,4 +95,38 @@ class CollectionTest extends TestCase
             "HR" => collect(["Budi"])
         ], $result->all());
     }
+
+    public function testZip()
+    {
+        $collection1 = collect(1, 2, 3);
+        $collection2 = collect(4, 5, 6);
+        $collection3 = $collection1->zip($collection2);
+
+        $this->assertEquals([
+            collect([1, 4]),
+            collect([2, 5]),
+            collect([3, 6]),
+        ], $collection3->all());
+    }
+
+    public function testConcat(): void
+    {
+        $collection1 = collect([1, 2, 3]);
+        $collection2 = collect([4, 5, 6]);
+        $collection3 = $collection1->concat($collection2);
+
+        $this->assertEqualsCanonicalizing([1, 2, 3, 4, 5, 6], $collection3->all());
+    }
+
+    public function testCombine(): void
+    {
+        $collection1 = collect(['name', 'country']);
+        $collection2 = collect(['Ivan', 'Indonesia']);
+        $collection3 = $collection1->combine($collection2);
+
+        $this->assertEqualsCanonicalizing([
+            'name' => 'Ivan',
+            'country' => 'Indonesia'
+        ], $collection3->all());
+    }
 }
