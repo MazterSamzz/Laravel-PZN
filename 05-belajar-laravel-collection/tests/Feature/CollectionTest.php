41cf6a304a82ffd75888ad5b782bd5c7f33b0a86
Samuel Ivan <maztersamzz@gmail.com>
2024-05-23 09:42:29 +0700

    Laravel Collection 06 Manipulasi Collection

--- 05-belajar-laravel-collection/tests/Feature/CollectionTest.php
+++ 05-belajar-laravel-collection/tests/Feature/CollectionTest.php
@@ -13,4 +13,24 @@ class CollectionTest extends TestCase
         $collection = collect([1, 2, 3]);
         $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
     }
+
+    public function testForEach(): void
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        foreach ($collection as $key => $value) {
+            $this->assertEquals($key + 1, $value);
+        }
+    }
+
+    public function testCrud(): void
+    {
+        $collection = collect([]);
+        $collection->push(1, 2, 3);
+        $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+
+        $result = $collection->pop();
+        $this->assertEquals(3, $result);
+        $this->assertEqualsCanonicalizing([1, 2], $collection->all());
+    }
 }
