4d8a1bc36d5a0166387883eb1bd01e753cbc4da3
Samuel Ivan <maztersamzz@gmail.com>
2024-05-25 20:19:02 +0700

    Laravel Database 13 Query Builder Select

--- belajar-laravel-database/tests/Feature/QueryBuilderTest.php
+++ belajar-laravel-database/tests/Feature/QueryBuilderTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 class QueryBuilderTest extends TestCase
@@ -32,5 +33,17 @@ class QueryBuilderTest extends TestCase
 
         $result = DB::select('select count(id) as total from categories');
         self::assertEquals(2, $result[0]->total);
+    }
+
+    public function testSelect(): void
+    {
+        $this->testInsert();
+
+        $collection = DB::table('categories')->select(['id', 'name'])->get();
+        self::assertNotNull($collection);
+
+        $collection->each(function ($item) {
+            Log::info(json_encode($item));
+        });
     }
 }
