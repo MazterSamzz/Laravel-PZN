f71d629dc6b8b5102be59fb3734c1de4dd8e94b8
Samuel Ivan <maztersamzz@gmail.com>
2024-05-25 22:45:26 +0700

    Laravel Database 14 Query Builder Update

--- belajar-laravel-database/tests/Feature/QueryBuilderTest.php
+++ belajar-laravel-database/tests/Feature/QueryBuilderTest.php
@@ -158,4 +158,50 @@ class QueryBuilderTest extends TestCase
             Log::info(json_encode($item));
         });
     }
+
+    public function testUpdate(): void
+    {
+        $this->insertCategories();
+
+        DB::table('categories')->where('id', '=', 'SMARTPHONE')
+            ->update([
+                'name' => 'Handphone',
+            ]);
+
+        $collection = DB::table('categories')->where('name', '=', 'Handphone')->get();
+        self::assertCount(1, $collection);
+    }
+
+    public function testUpdateOrInsert(): void
+    {
+        $this->insertCategories();
+
+        DB::table('categories')->updateOrInsert(
+            [
+                'id' => 'VOUCHER'
+            ],
+            [
+                'name' => 'Voucher',
+                'description' => 'Ticket and Voucher',
+                'created_at' => '2024-01-05 00:00:00'
+            ]
+        );
+
+        $collection = DB::table('categories')->where('id', '=', 'VOUCHER')->get();
+        self::assertCount(1, $collection);
+        $collection->each(function ($item) {
+            Log::info(json_encode($item));
+        });
+    }
+
+    public function testIncrement(): void
+    {
+        DB::table('counters')->where('id', '=', 'sample')->increment('counter', 1);
+
+        $collection = DB::table('counters')->where('id', '=', 'sample')->get();
+        self::assertCount(1, $collection);
+        $collection->each(function ($item) {
+            Log::info(json_encode($item));
+        });
+    }
 }
