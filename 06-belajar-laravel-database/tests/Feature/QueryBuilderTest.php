583dcc5fa5a9ce89eb14ac18ca94e5475d026115
Samuel Ivan <maztersamzz@gmail.com>
2024-05-26 21:32:19 +0700

    Laravel Database 24 Query Builder Raw

--- 06-belajar-laravel-database/tests/Feature/QueryBuilderTest.php
+++ 06-belajar-laravel-database/tests/Feature/QueryBuilderTest.php
@@ -344,4 +344,21 @@ class QueryBuilderTest extends TestCase
         $collection = DB::table('products')->sum('price');
         self::assertEquals(38000000, $collection);
     }
+
+    public function testQueryBuilderRaw(): void
+    {
+        $this->insertProducts();
+
+        $collection = DB::table('products')
+            ->select(
+                DB::raw('count(*) as total_product'),
+                DB::raw('min(price) as min_price'),
+                DB::raw('max(price) as max_price')
+            )->get();
+
+        echo $collection; // [{"total_product":2,"min_price":18000000,"max_price":20000000}]
+        self::assertEquals(2, $collection[0]->total_product);
+        self::assertEquals(18000000, $collection[0]->min_price);
+        self::assertEquals(20000000, $collection[0]->max_price);
+    }
 }
