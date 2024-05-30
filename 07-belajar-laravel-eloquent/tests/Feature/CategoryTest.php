50e0a2360146b28502b4cc598ec1976531314687
Samuel Ivan <maztersamzz@gmail.com>
2024-05-31 00:41:57 +0700

    Laravel Eloquent 09 Find

--- 07-belajar-laravel-eloquent/tests/Feature/CategoryTest.php
+++ 07-belajar-laravel-eloquent/tests/Feature/CategoryTest.php
@@ -40,4 +40,17 @@ class CategoryTest extends TestCase
         $total = Category::count();
         self::assertEquals(10, $total);
     }
+
+    public function testFind(): void
+    {
+        $this->seed("CategorySeeder");
+
+        // $category = Category::query()->find("FOOD");
+        $category = Category::find("FOOD");
+
+        self::assertNotNull($category);
+        self::assertEquals("FOOD", $category->id);
+        self::assertEquals("Food", $category->name);
+        self::assertEquals("Food Category", $category->description);
+    }
 }
