f76dd57899a308d96634852abcddff1a30644659
Samuel Ivan <maztersamzz@gmail.com>
2024-05-31 00:32:53 +0700

    Laravel Eloquent 08 Insert Many

--- 07-belajar-laravel-eloquent/tests/Feature/CategoryTest.php
+++ 07-belajar-laravel-eloquent/tests/Feature/CategoryTest.php
@@ -19,4 +19,25 @@ class CategoryTest extends TestCase
 
         self::assertTrue($result);
     }
+
+    public function testInsertManyCategories(): void
+    {
+        $categories = [];
+
+        for ($i = 0; $i < 10; $i++) {
+            $categories[] = [
+                "id" => "ID $i",
+                "name" => "Category $i"
+            ];
+        }
+
+        // $result = Category::query()->insert($categories);
+        $result = Category::insert($categories);
+
+        self::assertTrue($result);
+
+        // $total = Category::query()->count();
+        $total = Category::count();
+        self::assertEquals(10, $total);
+    }
 }
