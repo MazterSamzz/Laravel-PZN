917ffbda6986f28fe2a844b12e882026713556ba
Samuel Ivan <maztersamzz@gmail.com>
2024-05-30 22:38:07 +0700

    Laravel Eloquent 04 Membuat Database

---
+++ 07-belajar-laravel-eloquent/database.sql
@@ -0,0 +1,7 @@
+-- ======================== 04 Membuat Database ========================
+create database tutorial_laravel_pzn_laravel-eloquent;
+
+use tutorial_laravel_pzn_laravel-eloquent;
+
+show tables;
+-- ======================== End of Membuat Database ========================
