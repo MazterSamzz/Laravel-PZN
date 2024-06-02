11740f68f8bfe48c2a1e6af9d8e1fee1653ebe1c
Samuel Ivan <maztersamzz@gmail.com>
2024-06-02 22:21:40 +0700

    Laravel Eloquent 16 Timestamps

---
+++ 07-belajar-laravel-eloquent/tests/Feature/CommentTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Comment;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
+use Tests\TestCase;
+
+class CommentTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testCreateComment(): void
+    {
+        $comment = new Comment();
+
+        $comment->email = 'MazterSamzz@gmail.com';
+        $comment->title = 'Sample Title';
+        $comment->comment = ' Sample Comment ';
+
+        $comment->save();
+        Log::info(json_encode($comment));
+
+        self::assertNotNull($comment->id);
+    }
+}
