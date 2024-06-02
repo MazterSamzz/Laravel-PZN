01d878568ca6a63c005b6c29d9428899bbe24980
Samuel Ivan <maztersamzz@gmail.com>
2024-06-02 21:52:37 +0700

    Laravel Eloquent 15 UUID

---
+++ 07-belajar-laravel-eloquent/tests/Feature/VoucherTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Voucher;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
+use Tests\TestCase;
+
+class VoucherTest extends TestCase
+{
+    public function testCreateVoucher(): void
+    {
+        $voucher = new Voucher();
+
+        $voucher->name = 'Sample Voucher';
+        $voucher->voucher_code = '1231231234'; // 
+        $voucher->save();
+        Log::info(json_encode($voucher));
+
+        self::assertNotNull($voucher->id);
+    }
+
+    public function testVoucherCodeUUID(): void
+    {
+        $voucher = new Voucher();
+
+        $voucher->name = 'Sample Voucher';
+        $voucher->save();
+        Log::info(json_encode($voucher));
+
+        self::assertNotNull($voucher->id);
+        self::assertNotNull($voucher->voucher_code);
+    }
+}
