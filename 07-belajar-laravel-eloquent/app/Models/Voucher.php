e1b3adc5c88668bee2ed83929e8a180c9e3130ef
Samuel Ivan <8948689+MazterSamzz@users.noreply.github.com>
2024-06-06 21:06:28 +0700

    Laravel Eloquent 22 Query Local Scope

--- 07-belajar-laravel-eloquent/app/Models/Voucher.php
+++ 07-belajar-laravel-eloquent/app/Models/Voucher.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Builder;
 
 class Voucher extends Model
 {
@@ -18,5 +19,15 @@ class Voucher extends Model
     public function uniqueIds()
     {
         return [$this->primaryKey, 'voucher_code'];
+    }
+
+    public function scopeActive(Builder $builder)
+    {
+        $builder->where('is_active', true);
+    }
+
+    public function scopeNonActive(Builder $builder)
+    {
+        $builder->where('is_active', false);
     }
 }
