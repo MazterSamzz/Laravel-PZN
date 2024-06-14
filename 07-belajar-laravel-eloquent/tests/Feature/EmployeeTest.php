39343374ae3035c455dba364afb7ffe8250d0e1b
Jessica Acer <8948689+MazterSamzz@users.noreply.github.com>
2024-06-14 13:14:38 +0700

    Laravel Eloquent 47 Factory

---
+++ 07-belajar-laravel-eloquent/tests/Feature/EmployeeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Employee;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EmployeeTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $employee1 = Employee::factory()->programmer()->make();
+        $employee1->id = '1';
+        $employee1->name = 'Employee 1';
+        $employee1->save();
+
+        self::assertNotNull(Employee::where('id', '1')->first());
+
+        $employee2 = Employee::factory()->seniorProgrammer()->create([
+            'id' => '2',
+            'name' => 'Employee 2'
+        ]);
+        self::assertNotNull($employee2);
+        self::assertNotNull(Employee::where('id', '2')->first());
+    }
+}
