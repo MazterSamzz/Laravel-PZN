ecdc7bd01c7620e3949fcbf9df060e0cbeecd6fc
Samuel Ivan <8948689+MazterSamzz@users.noreply.github.com>
2024-06-16 08:23:52 +0700

    Laravel Validation 08 Validation Rules

--- 08-laravel-validation/tests/Feature/ValidatorTest.php
+++ 08-laravel-validation/tests/Feature/ValidatorTest.php
@@ -81,4 +81,26 @@ class ValidatorTest extends TestCase
             Log::error($message->toJson(JSON_PRETTY_PRINT));
         }
     }
+
+    public function testValidatorMultipleRules(): void
+    {
+        $data = [
+            'username' => "eko",
+            'password' => "eko"
+        ];
+
+        $rules = [
+            'username' => 'required|email|max:100',
+            'password' => ['required', 'min:6', 'max:20']
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+
+        self::assertFalse($validator->passes());
+        self::assertTrue($validator->fails());
+
+        $message = $validator->getMessageBag();
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
 }
