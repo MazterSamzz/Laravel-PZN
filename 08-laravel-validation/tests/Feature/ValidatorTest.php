0d38596e9ade9960de82418130ba8fe232898dd6
Samuel Ivan <8948689+MazterSamzz@users.noreply.github.com>
2024-06-16 07:55:54 +0700

    Laravel Validation 07 Validation Exception

--- 08-laravel-validation/tests/Feature/ValidatorTest.php
+++ 08-laravel-validation/tests/Feature/ValidatorTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
 class ValidatorTest extends TestCase
@@ -54,5 +55,30 @@ class ValidatorTest extends TestCase
         $message->get('username');
         $message->get('password');
         Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
+
+    public function testValidationException(): void
+    {
+        $data = [
+            'username' => '',
+            'password' => ''
+        ];
+
+        $rules = [
+            'username' => 'required',
+            'password' => 'required'
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+
+        try {
+            $validator->validate();
+            self::fail("ValidationException not thrown");
+        } catch (ValidationException $exception) {
+            self::assertNotNull($exception->validator);
+            $message = $exception->validator->errors();
+            Log::error($message->toJson(JSON_PRETTY_PRINT));
+        }
     }
 }
