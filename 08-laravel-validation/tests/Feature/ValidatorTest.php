2842b32a7ceac35997768d5f754e536a10db0d16
Samuel Ivan <8948689+MazterSamzz@users.noreply.github.com>
2024-06-16 07:35:43 +0700

    Laravel Validation 04 Validator

---
+++ 08-laravel-validation/tests/Feature/ValidatorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+
+class ValidatorTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_Validator(): void
+    {
+        $data = [
+            'username' => 'admin',
+            'password' => '123456'
+        ];
+
+        $rules = [
+            'username' => 'required',
+            'password' => 'required'
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+    }
+}
