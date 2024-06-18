ebc74dc37be0eb9e7db2c1d8f3edf0cfa801076e
Samuel Ivan <8948689+MazterSamzz@users.noreply.github.com>
2024-06-18 21:07:56 +0700

    Laravel Validation 16 HTTP Request Validation

--- 08-laravel-validation/routes/web.php
+++ 08-laravel-validation/routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/form/login', [App\Http\Controllers\FormController::class, 'login']);
