287a75323ccb13322a7cd3a0b64890f149bb28c0
Samuel Ivan <8948689+MazterSamzz@users.noreply.github.com>
2024-07-07 22:46:01 +0700

    Laravel HTTP Client 14 Throw Error

--- 14-laravel-http-client/tests/Feature/HttpTest.php
+++ 14-laravel-http-client/tests/Feature/HttpTest.php
@@ -4,33 +4,37 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class HttpTest extends TestCase
 {
+    // private $url = "https://enh047rcf2az6.x.pipedream.net";
+    private $url = "https://enayjtdq1a6bp.x.pipedream.net/";
+
     public function testGet(): void
     {
-        $response = Http::get("https://enh047rcf2az6.x.pipedream.net");
+        $response = Http::get($this->url);
         self::assertTrue($response->ok());
     }
 
     public function testPost(): void
     {
-        $response = Http::post("https://enh047rcf2az6.x.pipedream.net");
+        $response = Http::post($this->url);
         self::assertTrue($response->ok());
     }
 
 
     public function testDelete(): void
     {
-        $response = Http::delete("https://enh047rcf2az6.x.pipedream.net");
+        $response = Http::delete($this->url);
         self::assertTrue($response->ok());
     }
 
     public function testResponse()
     {
-        $response = Http::get("https://enh047rcf2az6.x.pipedream.net");
+        $response = Http::get($this->url);
         self::assertEquals(200, $response->status());
         self::assertNotNull($response->body());
 
@@ -43,7 +47,7 @@ class HttpTest extends TestCase
         $response = Http::withQueryParameters([
             "page" => 1,
             "limit" => 10
-        ])->get("https://enh047rcf2az6.x.pipedream.net");
+        ])->get($this->url);
         self::assertTrue($response->ok());
     }
 
@@ -55,7 +59,7 @@ class HttpTest extends TestCase
         ])->withHeaders([
             "accept" => "application/json",
             "x-request-ID" => '123123123',
-        ])->get("https://enh047rcf2az6.x.pipedream.net");
+        ])->get($this->url);
         self::assertTrue($response->ok());
     }
 
@@ -70,13 +74,13 @@ class HttpTest extends TestCase
         ])->withCookies([
             "SessionId" => "3242432423",
             "UserId" => "ivan"
-        ], "enh047rcf2az6.x.pipedream.net")->get("https://enh047rcf2az6.x.pipedream.net");
+        ], "enh047rcf2az6.x.pipedream.net")->get($this->url);
         self::assertTrue($response->ok());
     }
 
     public function testFormPost(): void
     {
-        $response = Http::asForm()->post("https://enh047rcf2az6.x.pipedream.net", [
+        $response = Http::asForm()->post($this->url, [
             "username" => "admin",
             "password" => "admin"
         ]);
@@ -88,7 +92,7 @@ class HttpTest extends TestCase
     {
         $response = Http::asMultipart()
             ->attach("profile", file_get_contents(__DIR__ . '/../../resources/HP.jpg'), "profile.jpg")
-            ->post("https://enh047rcf2az6.x.pipedream.net", [
+            ->post($this->url, [
                 "username" => "admin",
                 "password" => "admin"
             ]);
@@ -99,7 +103,7 @@ class HttpTest extends TestCase
     public function testJson(): void
     {
         $response = Http::asJson()
-            ->post("https://enh047rcf2az6.x.pipedream.net", [
+            ->post($this->url, [
                 "username" => "admin",
                 "password" => "admin"
             ]);
@@ -110,7 +114,7 @@ class HttpTest extends TestCase
     public function testTimeout(): void
     {
         $response = Http::timeout(5)->asJson()
-            ->post("https://enh047rcf2az6.x.pipedream.net", [
+            ->post($this->url, [
                 "username" => "admin",
                 "password" => "admin"
             ]);
@@ -121,11 +125,20 @@ class HttpTest extends TestCase
     public function testRetry(): void
     {
         $response = Http::timeout(1)->retry(5, 1000)->asJson()
-            ->post("https://enh047rcf2az6.x.pipedream.net", [
+            ->post($this->url, [
                 "username" => "admin",
                 "password" => "admin"
             ]);
 
         self::assertTrue($response->ok());
+    }
+
+    public function testThrowError(): void
+    {
+        $this->assertThrows(function () {
+            $response = Http::get("https://programmerzamannow.com/not-found");
+            self::assertEquals(404, $response->status());
+            $response->throw();
+        }, RequestException::class);
     }
 }
